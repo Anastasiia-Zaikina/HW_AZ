1621f37600220c725e3cdc81145cab7206011a01
Anastasiia <anastasi.zaikina@gmail.com>
2023-05-10 19:49:33 +0300

    task4_vegetarians

---
+++ homework2/task4_vegetarians.py
@@ -0,0 +1,12 @@
+#I decided just show a list of all guest since both groups can eat vegetables and herbs
+
+omnivores = ['Jenny', 'Lucas', 'Sveta', 'James']
+vegetarian = ['Olga', 'Igor']
+
+who_can_eat_vegetables_and_herbs = []
+
+who_can_eat_vegetables_and_herbs.extend(omnivores)
+who_can_eat_vegetables_and_herbs.extend(vegetarian)
+
+
+print(who_can_eat_vegetables_and_herbs)
