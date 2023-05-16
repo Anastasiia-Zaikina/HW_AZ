0eae4e37a699c6c6ea7f04aba13b7740d3fe33d7
Anastasiia <anastasi.zaikina@gmail.com>
2023-05-16 19:03:39 +0300

    task1.index_and_values

---
+++ homework3/task1.index_and_value.py
@@ -0,0 +1,10 @@
+elements = [1, 2, 3, 4, 5, 6, 7, 8]
+
+list_of_odd = []
+list_of_even = []
+
+for index, element in enumerate(elements):
+    if index % 2 != 0:
+     list_of_odd.append((index, element))
+    elif index % 2 == 0:
+        list_of_even.append((index, element))
