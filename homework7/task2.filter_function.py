03305058ef834d721b3bbea88d8fb34093a36fae
Anastasiia <anastasi.zaikina@gmail.com>
2023-06-01 22:35:40 +0300

    add_annotation

--- homework7/task2.filter_function.py
+++ homework7/task2.filter_function.py
@@ -1,15 +1,19 @@
 """Implement your realization of the function filter"""
 
 
-def filter_implementation(function, list_data: list):
+def filter_implementation(callback, sequence: list):
+    """
+    :param callback: function contains logic for element changes
+    :param sequence: incoming data
+    :return: list of updated elements
+    """
     filtered_list = []
-    for element in list_data:
-        if function(element):
+    for element in sequence:
+        if callback(element):
             filtered_list.append(element)
     return filtered_list
 
-
 if __name__ == '__main__':
     new_list = [1, 1, 2, 3, 4, 4]
-    equal_or_less_2 = filter_implementation(lambda x: x <= 2, new_list)
+    equal_or_less_2 = filter_implementation(lambda x: x <= 2, new_list)# Add it to show that I understand how to use this function
     print(equal_or_less_2)
